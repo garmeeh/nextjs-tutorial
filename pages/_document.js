691c7eeee89526216780031b0caf605e8a183de3
Gary Meehan <garymeehan@outlook.com>
2018-04-19 14:56:08 +0100

    add styled-components

---
+++ pages/_document.js
@@ -0,0 +1,32 @@
+/**
+ * _document is only rendered on the server side and not on the client
+ * side so event handlers like onClick can't be added to this file
+ */
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <title>My page</title>
+          {this.props.styleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
-const Index = () => <div>Hello World</div>;
+import styled from 'styled-components';
+
+const Hello = styled.div`
+  background: goldenrod;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 64px;
+  color: #102e37;
+`;
+
+const Index = () => <Hello>Hello World</Hello>;
 
 export default Index;
