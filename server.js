d936c379749e1e5397e70d73a11aeaa29cad6b50
Gary Meehan <garymeehan@outlook.com>
2018-04-19 13:53:45 +0100

    add server + production build

---
+++ server.js
@@ -0,0 +1,26 @@
+/**
+ *  More info 👉 https://github.com/zeit/next.js/#custom-server-and-routing
+ *  Note: This file doesn't go through babel or webpack transformation.
+ */
+const express = require('express');
+const dev = process.env.NODE_ENV !== 'production';
+const next = require('next');
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.get('*', (req, res) => handle(req, res));
+
+    server.listen(3000, err => {
+      if (err) throw err;
+      console.log('> Ready http://localhost:3000 <');
+    });
+  })
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
